796a9cc201fd0e40fcff9228802e5b50418d0cc8
Satchel Grant <grantsrb@stanford.edu>
2025-04-01 00:11:37 -0700

    updates

--- scripts/dispatch_exps.sh
+++ scripts/dispatch_exps.sh
@@ -2,15 +2,15 @@
 # Use this script to run the DAS experiments
 
 exp_name="mas_dispatch"
-#gpus=( 0 1 2 3 4 5 6 7 8 9 )
-gpus=( 6 7 8 9 )
+gpus=( 1 2 3 4 5 6 7 )
 root_folder="/mnt/fs2/grantsrb/mas_saves/"
-exp_folders1=( "big-multiobj"  ) #"big-multiobj" "lstm_multiobj" "tformer_multiobj_unk_d40" "big-sameobj" "lstm_sameobj" )
+exp_folders1=( "lstm_multiobj" "tformer_multiobj_unk_d40"   ) #"big-multiobj" "lstm_multiobj" "tformer_multiobj_unk_d40" "big-sameobj" "lstm_sameobj" )
 exp_folders2=( "big-multiobj" )
 config="configs/numequiv.yaml"
-n_units="n_units=24"
+n_units="n_units=48"
 fsr="fsr=False"
-consistent_targ_inpt_id="consistent_targ_inpt_id=False"
+const_targ_inpt_id="const_targ_inpt_id=False"
+mtx_types="mtx_types=RotationMatrix,RotationMatrix"
 
 echo Dispatching
 cuda_idx=0
@@ -30,7 +30,7 @@ do
                            model_path1="${exp_root1}/${model_folder1}"
                            model_path2="${exp_root2}/${model_folder2}"
 
-                           bash scripts/single_model_run.sh $cuda $exp_name $model_path1 $model_path2 $config $n_units $fsr $consistent_targ_inpt_id &
+                           bash scripts/single_model_run.sh $cuda $exp_name $model_path1 $model_path2 $config $n_units $fsr $const_targ_inpt_id $mtx_types &
 
                            cuda_idx=$((1+$cuda_idx))
                            if [[ ${cuda_idx} == ${#gpus[@]} ]]; then
