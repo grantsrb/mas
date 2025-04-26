384fca7dd97fb3d170a37fcb71a02f46c2d41320
Satchel Grant <grantsrb@stanford.edu>
2025-04-26 11:48:34 -0700

    add explicit stepwise arg

--- scripts/dispatch_exps.sh
+++ scripts/dispatch_exps.sh
@@ -2,15 +2,16 @@
 # Use this script to run the DAS experiments
 
 exp_name="mas_dispatch"
-gpus=( 1 2 3 4 5 6 7 )
+gpus=( 1 2 3 4 5 6 7 8 9 0 )
 root_folder="/mnt/fs2/grantsrb/mas_saves/"
-exp_folders1=( "lstm_multiobj" "tformer_multiobj_unk_d40"   ) #"big-multiobj" "lstm_multiobj" "tformer_multiobj_unk_d40" "big-sameobj" "lstm_sameobj" )
-exp_folders2=( "big-multiobj" )
+exp_folders1=( "lstm_multiobj" "tformer_multiobj_unk_d40" "big-sameobj" "lstm_sameobj" ) #"big-multiobj" "lstm_multiobj" "tformer_multiobj_unk_d40" "big-sameobj" "lstm_sameobj" )
+exp_folders2=( "lstm_multiobj" )
 config="configs/numequiv.yaml"
-n_units="n_units=48"
+n_units="n_units=16"
 fsr="fsr=False"
 const_targ_inpt_id="const_targ_inpt_id=False"
 mtx_types="mtx_types=RotationMatrix,RotationMatrix"
+stepwise="stepwise=True"
 
 echo Dispatching
 cuda_idx=0
@@ -30,7 +31,7 @@ do
                            model_path1="${exp_root1}/${model_folder1}"
                            model_path2="${exp_root2}/${model_folder2}"
 
-                           bash scripts/single_model_run.sh $cuda $exp_name $model_path1 $model_path2 $config $n_units $fsr $const_targ_inpt_id $mtx_types &
+                           bash scripts/single_model_run.sh $cuda $exp_name $model_path1 $model_path2 $config $n_units $fsr $const_targ_inpt_id $mtx_types $stepwise &
 
                            cuda_idx=$((1+$cuda_idx))
                            if [[ ${cuda_idx} == ${#gpus[@]} ]]; then
