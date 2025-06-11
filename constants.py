18a766547ed71f54ab627f54239ef67b0a6855ea
Satchel Grant <grantsrb@stanford.edu>
2025-06-11 00:04:02 -0700

    update gpt2 defaults

--- constants.py
+++ constants.py
@@ -25,13 +25,15 @@ PROMPTS = {
      ),
 
     "gpt2": (
-        "user_word For each trial, I am going to say \"D0\" some number "
-        "of times and then I want you to respond by saying \"R\" the exact "
-        "same number of times that I said \"D0\" in that trial. Please say "
-        "\"E\" at the end of your response to indicate that you are finished. "
-        "Do not say or think anything else.\nexample: user_word D0 T "
-        "R E \nexample: user_word D0 D0 T "
-        "R R E \nBegin trial: user_word "
+        f"user_word For each trial, I am going to "
+        f"say \"D0\" some number of times and then I want you to respond by "
+        f"saying \"R\" the exact same number of times that I said \"D0\" in "
+        f"that trial. Please say \"E\" at the end of your response to indicate "
+        f"that you are finished. Do not say or think anything else. "
+        f"\nexample: user_word D0 T R E "
+        f"\nexample: user_word D0 D0 T R R E "
+        f"\nexample: user_word D0 T R E "
+        f"\nexample: user_word"
     ),
 }
 
@@ -73,7 +75,7 @@ REPLACEMENTS = {
         "D2": "item",
         "T": ":",
         "asst_word": "",
-        "user_word": "User:",
+        "user_word": "",
         "R": "object",
         "E": "stop",
     },
@@ -82,7 +84,7 @@ REPLACEMENTS = {
 PADDING_SIDES = {
     "deepseek-ai/DeepSeek-R1-Distill-Llama-8B": "left",
     "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B": "left",
-    "gpt2": "left",
+    "gpt2": "right",
 }
 
 TASK2CMODEL = {
