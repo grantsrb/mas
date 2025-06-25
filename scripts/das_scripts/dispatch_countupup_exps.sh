7762be31476ad05fd68048a98df7ac4beb5cd3a2
Satchel Grant <grantsrb@stanford.edu>
2025-06-25 14:12:00 -0700

    update configs

--- scripts/das_scripts/dispatch_countupup_exps.sh
+++ scripts/das_scripts/dispatch_countupup_exps.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Use this script to run the DAS experiments
 
-exp_name="das"
+exp_name="upup"
 gpus=( 0 1 2 3 4 5 6 7 8 9 )
 root_folder="./models/"
 
-exp_folders1=( "multiobject_gru" ) #"multiobject_gru" ) # "sameobject_gru" "multiobject_lstm" "sameobject_lstm" "multiobjectmod_gru" )
+exp_folders1=( "multiobjectmod_gru" ) #"multiobject_gru" ) # "sameobject_gru" "multiobject_lstm" "sameobject_lstm" "multiobjectmod_gru" )
 configs=("configs/orthogonal_das.yaml" "configs/linear_das.yaml")
 search1=( "n_units=1" "n_units=2" "n_units=4" "n_units=16" "n_units=32" )
-search2=( "swap_keys=count" "swap_keys=phase" )
-arg1="cmodel_names=CountUpDown" # can also try cmodel_name=CountUpUp with swap_keys demo_count and resp_count
+search2=( "swap_keys=demo_count" "swap_keys=resp_count" "swap_keys=phase" )
+arg1="cmodel_names=CountUpUp"
 arg2=""
 arg3=""
 
