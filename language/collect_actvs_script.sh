954c8afb6118a056cd4e2e50c82b3a2cecbbad2b
Satchel Grant <grantsrb@stanford.edu>
2025-07-29 13:17:30 -0700

    minor changes

--- language/collect_actvs_script.sh
+++ language/collect_actvs_script.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-cuda_devices=0,1,2,3,5,7
-max_samples=400
+cuda_devices=8
+max_samples=1000
+dataset="anitamaxvim/jigsaw-toxic-comments"
 
-CUDA_VISIBLE_DEVICES=$cuda_devices python3 huggingface_generate_source_data.py\
-    dataset="anitamaxvim/jigsaw-toxic-comments"\
-    model_name="/data2/grantsrb/mas_finetunings/nontoxic_Anthropic-hh-rlhf_deepseek-ai-DeepSeek-R1-Distill-Qwen-1.5B/run_d2025-07-26_t16-31-08" \
-    max_samples=$max_samples
-CUDA_VISIBLE_DEVICES=$cuda_devices python3 huggingface_generate_source_data.py \
-    dataset="anitamaxvim/jigsaw-toxic-comments"\
-    model_name="/data2/grantsrb/mas_finetunings/toxic_Anthropic-hh-rlhf_deepseek-ai-DeepSeek-R1-Distill-Qwen-1.5B/run_d2025-07-26_t14-53-43" \
-    max_samples=$max_samples
+models=(
+    #"/mnt/fs2/grantsrb/deep_finetunes/nontoxic_anitamaxvim-jigsaw-toxic-comments_gpt2"
+    #"/mnt/fs2/grantsrb/deep_finetunes/nontoxic_lmsys-toxic-chat_gpt2"
+    #"/mnt/fs2/grantsrb/deep_finetunes/toxic_lmsys-toxic-chat_gpt2"
+    #"/mnt/fs2/grantsrb/deep_finetunes/toxic_anitamaxvim-jigsaw-toxic-comments_gpt2"
+    "/mnt/fs2/grantsrb/deep_finetunes/nontoxic_Anthropic-hh-rlhf_gpt2/"
+    "/mnt/fs2/grantsrb/deep_finetunes/toxic_Anthropic-hh-rlhf_gpt2/"
+)
 
-CUDA_VISIBLE_DEVICES=$cuda_devices python3 huggingface_generate_source_data.py\
-    dataset="anitamaxvim/jigsaw-toxic-comments"\
-    model_name="/data2/grantsrb/mas_finetunings/nontoxic_anitamaxvim-jigsaw-toxic-comments_deepseek-ai-DeepSeek-R1-Distill-Qwen-1.5B/run_d2025-07-26_t13-16-04" \
-    max_samples=$max_samples
-CUDA_VISIBLE_DEVICES=$cuda_devices  python3 huggingface_generate_source_data.py \
-    dataset="anitamaxvim/jigsaw-toxic-comments"\
-    model_name="/data2/grantsrb/mas_finetunings/toxic_anitamaxvim-jigsaw-toxic-comments_deepseek-ai-DeepSeek-R1-Distill-Qwen-1.5B/run_d2025-07-26_t11-40-08" \
-    max_samples=$max_samples
-
-CUDA_VISIBLE_DEVICES=$cuda_devices  python3 huggingface_generate_source_data.py\
-    dataset="anitamaxvim/jigsaw-toxic-comments"\
-    model_name="/data2/grantsrb/mas_finetunings/nontoxic_lmsys-toxic-chat_deepseek-ai-DeepSeek-R1-Distill-Qwen-1.5B/run_d2025-07-26_t21-17-25" \
-    max_samples=$max_samples
-CUDA_VISIBLE_DEVICES=$cuda_devices  python3 huggingface_generate_source_data.py \
-    dataset="anitamaxvim/jigsaw-toxic-comments"\
-    model_name="/data2/grantsrb/mas_finetunings/toxic_lmsys-toxic-chat_deepseek-ai-DeepSeek-R1-Distill-Qwen-1.5B/run_d2025-07-26_t18-08-00" \
-    max_samples=$max_samples
+for model in ${models[@]}
+do
+    echo CUDA_VISIBLE_DEVICES=$cuda_devices python3 huggingface_collect_actvs.py\
+        dataset=$dataset\
+        max_samples=$max_samples\
+        model_name="${model}"\
+        temperature=0\
+        top_p=1\
+        $1 $2 $3 $4
+    CUDA_VISIBLE_DEVICES=$cuda_devices python3 huggingface_collect_actvs.py\
+        dataset=$dataset\
+        model_name="${model}"\
+        max_samples=$max_samples\
+        temperature=0\
+        top_p=1\
+        $1 $2 $3 $4
+done
 
